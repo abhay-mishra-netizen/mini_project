5cda5c871e557cc6c1136c4b41c4b88304102153
abbasMkhan <richreformer5018.c@gmail.com>
2026-04-03 18:11:27 +0530

    adding new file

--- src/fl_baseline.py
+++ src/fl_baseline.py
@@ -1,70 +1,116 @@
+import copy
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import copy
 
-def local_train( model , dataloader , epochs , lr = 0.01 , device = "cuda") :
+
+def local_train(model, dataloader, epochs, lr=0.01, device="cuda"):
+    model = model.to(device)
     model.train()
-    model.to(device)
-    optimizer = optim.SGD( model.parameter() , lr=lr)
-    criterion = nn.CrossEntropyLoss()
-    for i in range(epochs) :
-        for data , target in dataloader :
-            data , target = data.to(device) , target.to(device)
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion( output , target )
-        loss.backward()
-        optimizer.step()
-        return model.state_dict()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+
+    total_loss = 0.0
+    total_correct = 0
+    total_seen = 0
+
+    for _ in range(epochs):
+        for data, target in dataloader:
+            data = data.to(device)
+            target = target.to(device)
+
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item() * target.size(0)
+            total_correct += (output.argmax(dim=1) == target).sum().item()
+            total_seen += target.size(0)
+
+    avg_loss = total_loss / total_seen if total_seen > 0 else 0.0
+    avg_acc = total_correct / total_seen if total_seen > 0 else 0.0
+
+    return copy.deepcopy(model.state_dict()), avg_loss, avg_acc, total_seen
+
 
 def average_weights(weights_list, data_sizes):
     avg_weights = copy.deepcopy(weights_list[0])
     total_data = sum(data_sizes)
+
     for key in avg_weights.keys():
         avg_weights[key] = weights_list[0][key] * (data_sizes[0] / total_data)
 
         for i in range(1, len(weights_list)):
             avg_weights[key] += weights_list[i][key] * (data_sizes[i] / total_data)
+
     return avg_weights
 
+
 def fedavg(global_model, client_loaders, num_rounds, local_epochs, lr, device):
-           global_model = global_model.to(device)
-           for r in range(num_rounds) :
-                print(f"Round {r+1}/{num_rounds}")
-                local_weights = []
-           for loader in client_loaders:
+    global_model = global_model.to(device)
+    history = []
+
+    for round_idx in range(num_rounds):
+        print(f"[FL] Round {round_idx + 1}/{num_rounds}")
+
+        local_weights = []
+        data_sizes = []
+        round_losses = []
+        round_accs = []
+
+        for loader in client_loaders:
             local_model = copy.deepcopy(global_model)
 
-            weights = local_train(
+            weights, train_loss, train_acc, num_samples = local_train(
                 local_model,
                 loader,
                 epochs=local_epochs,
                 lr=lr,
-                device=device
+                device=device,
             )
+
             local_weights.append(weights)
-            new_weights = average_weights(local_weights)
-            global_model.load_state_dict(new_weights)
-            return global_model
-           
+            data_sizes.append(num_samples)
+            round_losses.append(train_loss)
+            round_accs.append(train_acc)
+
+        new_weights = average_weights(local_weights, data_sizes)
+        global_model.load_state_dict(new_weights)
+
+        history.append({
+            "round": round_idx + 1,
+            "train_loss": sum(round_losses) / len(round_losses),
+            "train_acc": sum(round_accs) / len(round_accs),
+        })
+
+    return global_model, history
+
+
 def evaluate(model, dataloader, device="cpu"):
     model.eval()
     model = model.to(device)
 
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
     correct = 0
     total = 0
 
     with torch.no_grad():
         for data, target in dataloader:
-            data, target = data.to(device), target.to(device)
+            data = data.to(device)
+            target = target.to(device)
 
             outputs = model(data)
-            _, predicted = torch.max(outputs, 1)
+            loss = criterion(outputs, target)
 
+            total_loss += loss.item() * target.size(0)
+            correct += (outputs.argmax(dim=1) == target).sum().item()
             total += target.size(0)
-            correct += (predicted == target).sum().item()
 
-    accuracy = 100 * correct / total
-    return accuracy          
+    avg_loss = total_loss / total if total > 0 else 0.0
+    accuracy = correct / total if total > 0 else 0.0
+    return avg_loss, accuracy
