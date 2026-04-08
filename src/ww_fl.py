4dcadac61c2f827833ae5e9a759ea5dd43f53ec8
abbasMkhan <richreformer5018.c@gmail.com>
2026-04-08 21:00:18 +0530

    wwfl arch

--- src/ww_fl.py
+++ src/ww_fl.py
@@ -1,250 +1,194 @@
 import copy
-import random
+from typing import Dict, List, Tuple
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import ConcatDataset, DataLoader
 
 
-class WWFLTrainer:
-    def __init__(
-        self,
-        model_fn,
-        client_datasets,
-        cluster_map,
-        test_loader,
-        num_rounds=500,
-        clients_per_cluster_per_round=10,
-        local_epochs=5,
-        batch_size=80,
-        lr=0.05,
-        device="cuda",
-        seed=42,
-        dataset_name='mnist'
-    ):
-        self.model_fn = model_fn
-        self.client_datasets = client_datasets
-        self.cluster_map = cluster_map
-        self.test_loader = test_loader
-        self.num_rounds = num_rounds
-        self.clients_per_cluster_per_round = \
-            clients_per_cluster_per_round
-        self.local_epochs = local_epochs
-        self.batch_size = batch_size
-        self.lr = lr
-        self.device = device
-        self.seed = seed
-        self.dataset_name = dataset_name
+def train_cluster_model(
+    model: nn.Module,
+    dataloader,
+    epochs: int,
+    lr: float = 0.05,
+    device: str = "cpu",
+) -> Tuple[Dict[str, torch.Tensor], float, float, int]:
+    model = model.to(device)
+    model.train()
 
-        
-        self.global_model = self.model_fn().to(device)
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=lr,
+        momentum=0.9,
+        weight_decay=5e-4,
+    )
+    criterion = nn.CrossEntropyLoss()
 
-        
-        self.cluster_memory = {
-            cluster_id: []
-            for cluster_id in self.cluster_map
-        }
-        self.history = []
+    total_loss = 0.0
+    total_correct = 0
+    total_seen = 0
 
-    def _sample_clients(self, cluster_id, round_idx):
-     
-        rng = random.Random(
-            self.seed + 1000 * round_idx + cluster_id
-        )
-        client_ids = self.cluster_map[cluster_id]
-        k = min(
-            self.clients_per_cluster_per_round,
-            len(client_ids)
-        )
-        return rng.sample(client_ids, k)
+    for _ in range(epochs):
+        for data, target in dataloader:
+            data = data.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
 
-    def _get_cluster_dataset(self, cluster_id,
-                              selected_client_ids):
-        
-        new_parts = [
-            self.client_datasets[cid]
-            for cid in selected_client_ids
-        ]
-        self.cluster_memory[cluster_id].extend(new_parts)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
 
-       
-        max_parts = self.clients_per_cluster_per_round * 20
-        if len(self.cluster_memory[cluster_id]) > max_parts:
-            self.cluster_memory[cluster_id] = \
-                self.cluster_memory[cluster_id][-max_parts:]
+            batch_size = target.size(0)
+            total_loss += loss.item() * batch_size
+            total_correct += (output.argmax(dim=1) == target).sum().item()
+            total_seen += batch_size
 
-        if len(self.cluster_memory[cluster_id]) == 1:
-            return self.cluster_memory[cluster_id][0]
-        return ConcatDataset(self.cluster_memory[cluster_id])
+    avg_loss = total_loss / total_seen if total_seen > 0 else 0.0
+    avg_acc = total_correct / total_seen if total_seen > 0 else 0.0
 
-    def _train_cluster_model(self, cluster_dataset):
-    
+    return copy.deepcopy(model.state_dict()), avg_loss, avg_acc, total_seen
 
-        model = self.model_fn().to(self.device)
 
-   
-        model.load_state_dict(
-            copy.deepcopy(self.global_model.state_dict())
-        )
-        model.train()
+def aggregate_cluster_models(
+    weights_list: List[Dict[str, torch.Tensor]],
+    data_sizes: List[int],
+) -> Dict[str, torch.Tensor]:
+    if not weights_list:
+        raise ValueError("weights_list cannot be empty")
 
-      
-        loader = DataLoader(
-            cluster_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=0,
-            pin_memory=(self.device == 'cuda')
-        )
+    total_data = sum(data_sizes)
+    if total_data == 0:
+        raise ValueError("Total data size cannot be zero")
 
-        optimizer = optim.SGD(
-            model.parameters(),
-            lr=self.lr,
-            momentum=0.9
-        )
-        criterion = nn.CrossEntropyLoss()
+    aggregated = copy.deepcopy(weights_list[0])
 
-        total_loss = 0.0
-        total_correct = 0
-        total_seen = 0
+    for key in aggregated.keys():
+        aggregated[key] = weights_list[0][key] * (data_sizes[0] / total_data)
+        for idx in range(1, len(weights_list)):
+            aggregated[key] += weights_list[idx][key] * (data_sizes[idx] / total_data)
 
-        for _ in range(self.local_epochs):
-            for data, target in loader:
-                data = data.to(self.device)
-                target = target.to(self.device)
+    return aggregated
 
-                optimizer.zero_grad()
-                output = model(data)
-                loss = criterion(output, target)
-                loss.backward()
-                optimizer.step()
 
-                total_loss += loss.item() * target.size(0)
-                total_correct += (
-                    output.argmax(dim=1) == target
-                ).sum().item()
-                total_seen += target.size(0)
+def evaluate(model: nn.Module, dataloader, device: str = "cpu") -> Tuple[float, float]:
+    model = model.to(device)
+    model.eval()
 
-        avg_loss = total_loss / total_seen \
-                   if total_seen > 0 else 0.0
-        avg_acc = total_correct / total_seen \
-                  if total_seen > 0 else 0.0
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    total_correct = 0
+    total_seen = 0
 
-        # return weights to global servers
-        
-        return (
-            copy.deepcopy(model.state_dict()),
-            len(cluster_dataset),
-            avg_loss,
-            avg_acc
-        )
+    with torch.no_grad():
+        for data, target in dataloader:
+            data = data.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
 
-    def _aggregate_cluster_models(self, cluster_states,
-                                   cluster_sizes):
-      
-        new_state = copy.deepcopy(cluster_states[0])
-        total_size = sum(cluster_sizes)
+            output = model(data)
+            loss = criterion(output, target)
 
-        for key in new_state.keys():
-            new_state[key] = cluster_states[0][key] * \
-                             (cluster_sizes[0] / total_size)
-            for i in range(1, len(cluster_states)):
-                new_state[key] += \
-                    cluster_states[i][key] * \
-                    (cluster_sizes[i] / total_size)
+            batch_size = target.size(0)
+            total_loss += loss.item() * batch_size
+            total_correct += (output.argmax(dim=1) == target).sum().item()
+            total_seen += batch_size
 
-        # update global model
-        # in real WW-FL global model stays secret shared
-        # never revealed to cluster servers or clients
-        self.global_model.load_state_dict(new_state)
+    avg_loss = total_loss / total_seen if total_seen > 0 else 0.0
+    avg_acc = total_correct / total_seen if total_seen > 0 else 0.0
+    return avg_loss, avg_acc
 
-    def evaluate(self):
-        # evaluation on test set
-       
-        self.global_model.eval()
-        criterion = nn.CrossEntropyLoss()
 
-        total_loss = 0.0
-        total_correct = 0
-        total_seen = 0
+def wwfl_train(
+    global_model: nn.Module,
+    data_manager,
+    test_loader,
+    num_rounds: int,
+    local_epochs: int,
+    lr: float,
+    device: str,
+    on_round_end=None,
+):
+    global_model = global_model.to(device)
+    round_history = []
+    cluster_history = []
 
-        with torch.no_grad():
-            for data, target in self.test_loader:
-                data = data.to(self.device)
-                target = target.to(self.device)
+    for round_idx in range(1, num_rounds + 1):
+        print(f"[WWFL] Round {round_idx}/{num_rounds}")
+        round_clusters = data_manager.prepare_round_cluster_loaders()
+        current_round_cluster_rows = []
 
-                output = self.global_model(data)
-                loss = criterion(output, target)
+        cluster_weights = []
+        cluster_sizes = []
+        weighted_losses = []
+        weighted_accs = []
 
-                total_loss += loss.item() * target.size(0)
-                total_correct += (
-                    output.argmax(dim=1) == target
-                ).sum().item()
-                total_seen += target.size(0)
+        for cluster_id in sorted(round_clusters.keys()):
+            cluster_info = round_clusters[cluster_id]
+            cluster_model = copy.deepcopy(global_model)
 
-        avg_loss = total_loss / total_seen \
-                   if total_seen > 0 else 0.0
-        avg_acc = total_correct / total_seen \
-                  if total_seen > 0 else 0.0
-        return avg_loss, avg_acc
-
-    def fit(self):
-        for round_idx in range(1, self.num_rounds + 1):
-            print(
-                f"[WW-FL] Round {round_idx}/{self.num_rounds}"
+            weights, train_loss, train_acc, num_samples = train_cluster_model(
+                model=cluster_model,
+                dataloader=cluster_info["loader"],
+                epochs=local_epochs,
+                lr=lr,
+                device=device,
             )
 
-            cluster_states = []
-            cluster_sizes = []
-            cluster_losses = []
-            cluster_accs = []
+            cluster_weights.append(weights)
+            cluster_sizes.append(num_samples)
+            weighted_losses.append(train_loss * num_samples)
+            weighted_accs.append(train_acc * num_samples)
 
-            
-            for cluster_id in sorted(
-                self.cluster_map.keys()
-            ):
-                selected_client_ids = self._sample_clients(
-                    cluster_id, round_idx
-                )
-                cluster_dataset = self._get_cluster_dataset(
-                    cluster_id, selected_client_ids
-                )
-
-                state_dict, cluster_size, \
-                train_loss, train_acc = \
-                    self._train_cluster_model(
-                        cluster_dataset
-                    )
-
-                cluster_states.append(state_dict)
-                cluster_sizes.append(cluster_size)
-                cluster_losses.append(train_loss)
-                cluster_accs.append(train_acc)
-
-            # global aggregation after all clusters finish
-            self._aggregate_cluster_models(
-                cluster_states, cluster_sizes
-            )
-            test_loss, test_acc = self.evaluate()
-
-            round_info = {
+            cluster_row = {
                 "round": round_idx,
-                "cluster_train_loss": sum(cluster_losses) /
-                                      len(cluster_losses),
-                "cluster_train_acc": sum(cluster_accs) /
-                                     len(cluster_accs),
-                "test_loss": test_loss,
-                "test_acc": test_acc,
+                "cluster_id": cluster_id,
+                "sampled_clients": len(cluster_info["sampled_client_ids"]),
+                "new_clients_added": len(cluster_info["new_client_ids"]),
+                "cluster_dataset_size": num_samples,
+                "train_loss": train_loss,
+                "train_acc": train_acc,
             }
-            self.history.append(round_info)
+            current_round_cluster_rows.append(cluster_row)
 
             print(
-                f"cluster_loss="
-                f"{round_info['cluster_train_loss']:.4f} "
-                f"cluster_acc="
-                f"{round_info['cluster_train_acc']:.4f} "
-                f"test_loss={test_loss:.4f} "
-                f"test_acc={test_acc:.4f}"
+                f"  cluster={cluster_id:02d} "
+                f"sampled={len(cluster_info['sampled_client_ids'])} "
+                f"new={len(cluster_info['new_client_ids'])} "
+                f"size={num_samples} "
+                f"train_loss={train_loss:.4f} "
+                f"train_acc={train_acc:.4f}"
             )
 
-        return self.global_model, self.history
+        aggregated_weights = aggregate_cluster_models(cluster_weights, cluster_sizes)
+        global_model.load_state_dict(aggregated_weights)
+
+        total_samples = sum(cluster_sizes)
+        cluster_train_loss = sum(weighted_losses) / total_samples if total_samples > 0 else 0.0
+        cluster_train_acc = sum(weighted_accs) / total_samples if total_samples > 0 else 0.0
+        test_loss, test_acc = evaluate(global_model, test_loader, device=device)
+
+        round_row = {
+            "round": round_idx,
+            "cluster_train_loss": cluster_train_loss,
+            "cluster_train_acc": cluster_train_acc,
+            "test_loss": test_loss,
+            "test_acc": test_acc,
+        }
+        round_history.append(round_row)
+        for cluster_row in current_round_cluster_rows:
+            cluster_row["test_loss"] = test_loss
+            cluster_row["test_acc"] = test_acc
+        cluster_history.extend(current_round_cluster_rows)
+
+        print(
+            f"  summary round={round_idx} "
+            f"cluster_train_loss={cluster_train_loss:.4f} "
+            f"cluster_train_acc={cluster_train_acc:.4f} "
+            f"test_loss={test_loss:.4f} "
+            f"test_acc={test_acc:.4f}"
+        )
+        if on_round_end is not None:
+            on_round_end(global_model, round_history, cluster_history)
+
+    return global_model, round_history, cluster_history
+
